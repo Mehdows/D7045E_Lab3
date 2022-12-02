907e4a0c595e73dc41c68003b07c435f26d7380b
Mehdows <marcus99.ma@gmail.com>
2022-12-02 15:26:39 +0100

    camera finished

--- camera.js
+++ camera.js
@@ -1,12 +1,24 @@
 class Camera {
-    constructor(gl){
+    constructor(gl, shaderProgram) {
         this.gl = gl;
-        this.viewMatrix = mat4.create();
-        this.projectionMatrix = mat4.create();
+        this.shaderProgram = shaderProgram;
 
-        let aspect = this.gl.canvas.clientWidth / this.gl.canvas.clientHeight;
-        let near = 0.1;
-        let far = 1000;
+        let eye = vec3(0, 0, 0);
+        let at = vec3(0, 0, 0);
+        let up = vec3(0, 1, 0);
+
+        let aspect = canvas.width / canvas.height;
+
+        this.viewMatrix = lookAt(eye, at, up);
+        this.projectionMatrix = perspective(45, aspect, 0.1, 100);
+
+    }
+
+    activate(){
+        let projMatrix = this.gl.getUniformLocation(this.shaderProgram, "projectionMatrix");
+        let viewMatrix = this.gl.getUniformLocation(this.shaderProgram, "viewMatrix");
+        this.gl.uniformMatrix4fv(projMatrix, false, this.projectionMatrix);
+        this.gl.uniformMatrix4fv(viewMatrix, false, this.viewMatrix);
     }
 
 }
