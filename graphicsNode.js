c58affe942993125764fd737b6918d1f07d5fa65
Andreas <Formarn3@gmail.com>
2022-12-02 15:41:31 +0100

    blabla

--- graphicsNode.js
+++ graphicsNode.js
@@ -6,11 +6,13 @@ class GraphicsNode {
         this.transform = transform;
     }
 
+    // Draw the node
     draw() {
         this.material.applyMaterial(this.transform);
         this.gl.drawElements(this.gl.TRIANGLES, this.mesh.getIndices().length, this.gl.UNSIGNED_SHORT, 0);
     }
 
+    // Update the green node position
     update(transform) {
         this.transform = transform;
     }
